8a0a6d18265f6c0b850239dfe00f577ff32c9db6
Joao Santos <mrburgbr3000@gmail.com>
2025-11-26 12:41:14 -0300

    começo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Header } from "@/components/Header";
+
 export default function HomePage() {
-    return <h1>Olá de dentro de page.tsx</h1>;
+    return (
+        <div>
+            <Header />
+        </div>
+    );
 }
